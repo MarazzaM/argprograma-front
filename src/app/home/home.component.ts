446fcdade2b365a72e4ebd8c447982074425c299
MarazzaM <marazza.matias@gmail.com>
2023-05-11 00:27:02 -0300

    test(always come back if needed)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,345 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { User } from '../model/user';
+import { UserService } from '../service/user.service';
+import { NotificationService } from '../service/notification.service';
+import { NotificationType } from '../enum/notification-type.enum';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AuthenticationService } from '../service/authentication.service';
+import { Router } from '@angular/router';
+import { FileUploadStatus } from '../model/file-upload.status';
+import { Role } from '../enum/role.enum';
+import Typed from 'typed.js';
+import AOS from 'aos';
+import Swiper from 'swiper';
+import GLightbox from 'glightbox';
+import Isotope from 'isotope-layout';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  private titleSubject = new BehaviorSubject<string>('Users');
+  public titleAction$ = this.titleSubject.asObservable();
+  public users: User[];
+  public user: User;
+  public refreshing: boolean;
+  public selectedUser: User;
+  public fileName: string;
+  public profileImage: File;
+  private subscriptions: Subscription[] = [];
+  public editUser = new User();
+  private currentUsername: string;
+  public fileStatus = new FileUploadStatus();
 
+  constructor(private router: Router, private authenticationService: AuthenticationService,
+              private userService: UserService, private notificationService: NotificationService) {}
+  ngOnInit(): void {
+    this.user = this.authenticationService.getUserFromLocalCache();
+    this.getUsers(true);
+
+    const options = {
+      strings: ['Developer.', 'Freelancer.','RPA Developer', 'Student'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      showCursor: true,
+      cursorChar: '|',
+      loop: true
+    };
+    const typed = new Typed('.typed', options);
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+
+
+ /**
+     * Easy selector helper function
+     */
+ const select = (el, all = false) => {
+  el = el.trim()
+  if (all) {
+    return Array.from(document.querySelectorAll(el))
+  } else {
+    return document.querySelector(el)
+  }
+}
+
+/**
+ * Easy event listener function
+ */
+const on = (type, el, listener, all = false) => {
+  let selectEl = select(el, all)
+  if (selectEl) {
+    if (all) {
+      selectEl.forEach(e => e.addEventListener(type, listener))
+    } else {
+      selectEl.addEventListener(type, listener)
+    }
+  }
+}
+
+/**
+ * Easy on scroll event listener
+ */
+const onscroll = (el, listener) => {
+  el.addEventListener('scroll', listener)
+}
+
+/**
+ * Navbar links active state on scroll
+ */
+let navbarlinks = select('#navbar .scrollto', true)
+const navbarlinksActive = () => {
+  let position = window.scrollY + 200
+  navbarlinks.forEach(navbarlink => {
+    if (!navbarlink.hash) return
+    let section = select(navbarlink.hash)
+    if (!section) return
+    if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+      navbarlink.classList.add('active')
+    } else {
+      navbarlink.classList.remove('active')
+    }
+  })
+}
+window.addEventListener('load', navbarlinksActive)
+onscroll(document, navbarlinksActive)
+
+/**
+ * Scrolls to an element with header offset
+ */
+const scrollto = (el) => {
+  let elementPos = select(el).offsetTop
+  window.scrollTo({
+    top: elementPos,
+    behavior: 'smooth'
+  })
+}
+
+/**
+ * Back to top button
+ */
+let backtotop = select('.back-to-top')
+if (backtotop) {
+  const toggleBacktotop = () => {
+    if (window.scrollY > 100) {
+      backtotop.classList.add('active')
+    } else {
+      backtotop.classList.remove('active')
+    }
+  }
+  window.addEventListener('load', toggleBacktotop)
+  onscroll(document, toggleBacktotop)
+}
+
+/**
+ * Mobile nav toggle
+ */
+on('click', '.mobile-nav-toggle', function(e) {
+  var header = select('#header');
+  header.classList.toggle('mobile-nav-active');
+  if (header.classList.contains('mobile-nav-active')) {
+    header.style.left = '0';
+  } else {
+    header.style.left = '-300px';
+    if (window.innerWidth >= 1200) {
+      header.style.left = '0';
+    }
+  }
+  this.classList.toggle('bi-list');
+  this.classList.toggle('bi-x');
+})
+
+
+
+/**
+ * Scrool with ofset on links with a class name .scrollto
+ */
+on('click', '.scrollto', function(e) {
+  if (select(this.hash)) {
+    e.preventDefault()
+
+    let body = select('body')
+    if (body.classList.contains('mobile-nav-active')) {
+      body.classList.remove('mobile-nav-active')
+      let navbarToggle = select('.mobile-nav-toggle')
+      navbarToggle.classList.toggle('bi-list')
+      navbarToggle.classList.toggle('bi-x')
+    }
+    scrollto(this.hash)
+  }
+}, true)
+
+
+/**
+   * Scroll with ofset on page load with hash links in the url
+   */
+window.addEventListener('load', () => {
+  if (window.location.hash) {
+    if (select(window.location.hash)) {
+      scrollto(window.location.hash)
+    }
+  }
+});
+
+
+
+
+
+
+/**
+ * Initiate portfolio lightbox 
+ */
+const portfolioLightbox = GLightbox({
+  selector: '.portfolio-lightbox'
+});
+
+/**
+ * Portfolio details slider
+ */
+new Swiper('.portfolio-details-slider', {
+  speed: 400,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  }
+});
+
+/**
+ * Testimonials slider
+ */
+new Swiper('.testimonials-slider', {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    }
+  }
+});
+
+/**
+ * Animation on scroll
+ */
+window.addEventListener('load', () => {
+  AOS.init({
+    duration: 1000,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false
+  })
+});
+    /**
+     * Portfolio isotope and filter
+     */
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function (e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function (el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function () {
+          AOS.refresh();
+        });
+      }, true);
+    }
+
+
+  }
+
+  public getUsers(showNotification: boolean): void {
+    this.refreshing = true;
+    this.subscriptions.push(
+      this.userService.getUsers().subscribe(
+        (response: User[]) => {
+          this.userService.addUsersToLocalCache(response);
+          this.users = response;
+          this.refreshing = false;
+          if (showNotification) {
+            // this.sendNotification(NotificationType.SUCCESS, `${response.length} user(s) loaded successfully.`);
+          }
+        },
+        (errorResponse: HttpErrorResponse) => {
+          this.sendNotification(NotificationType.ERROR, errorResponse.error.message);
+          this.refreshing = false;
+        }
+      )
+    );
+
+  }
+
+  public onLogOut(): void {
+    this.authenticationService.logOut();
+    location.reload();
+    this.sendNotification(NotificationType.SUCCESS, `You've been successfully logged out`);
+  }
+
+  public get isAdmin(): boolean {
+    return this.getUserRole() === Role.ADMIN || this.getUserRole() === Role.SUPER_ADMIN;
+  }
+
+  public get isManager(): boolean {
+    return this.isAdmin || this.getUserRole() === Role.MANAGER;
+  }
+
+  public get isAdminOrManager(): boolean {
+    return this.isAdmin || this.isManager;
+  }
+
+  private getUserRole(): string {
+    return this.authenticationService.getUserFromLocalCache().role;
+  }
+
+  private sendNotification(notificationType: NotificationType, message: string): void {
+    if (message) {
+      this.notificationService.notify(notificationType, message);
+    } else {
+      this.notificationService.notify(notificationType, 'An error occurred. Please try again.');
+    }
+  }
+
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
 }
